fc35e192103b7c02a04b2cbf8ecde81e5f7afa88
ctung <chuck.tung@gmail.com>
2020-04-29 09:35:56 -0400

    Update Fifo.h
    comments

--- vent_systest1/Fifo.h
+++ vent_systest1/Fifo.h
@@ -1,3 +1,10 @@
+/* 
+ *  Fifo.h - Library for a custom ring buffer with a user specified depth
+ *  It tracks the average of all entries
+ *  It can also track the derivative of incoming measurements
+ *  and report if that dervative is converging to zero or diverging
+ */
+
 #ifndef Fifo_h
 #define Fifo_h
 
